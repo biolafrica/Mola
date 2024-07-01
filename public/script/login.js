f674c3c921b952fd1b8c5e593e0c4dc35ec590b3
biolafrica <biolafrica@gmail.com>
2024-07-01 12:29:21 +0100

    handle login errror message

--- public/script/login.js
+++ public/script/login.js
@@ -3,6 +3,8 @@ form.addEventListener("submit", async(e)=>{
   e.preventDefault();
   let email_address= form.email.value;
   let password = form.password.value;
+  let emailErrorHTML = document.querySelector(".email_sub_text");
+  let passwordErrorHTML = document.querySelector(".password_sub_text");
 
   try {
     response = await fetch("http://127.0.0.1:8000/api/login/", {
@@ -22,6 +24,12 @@ form.addEventListener("submit", async(e)=>{
       localStorage.setItem("access", data.access);
       localStorage.setItem("refresh", data.refresh);
       location.assign("../index.html");
+    }else{
+      let errorMessage = data.non_field_errors[0];
+      emailErrorHTML.innerHTML = errorMessage;
+      passwordErrorHTML.innerHTML = errorMessage;
+
+
     }
     
   } catch (error) {
