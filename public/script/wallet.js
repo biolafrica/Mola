5b08a9a593613031a702a0e1dfb1244aaa73893e
biolafrica <biolafrica@gmail.com>
2024-08-19 14:46:09 +0100

    wallet page html and css implementation and responsiveness

--- public/script/wallet.js
+++ public/script/wallet.js
@@ -1,16 +1,3 @@
-const subMenuEl = document.querySelector(".js_sub_menu");
-const hamburgerBtn = document.querySelector(".js_hamburger");
-const cancelBtn = document.querySelector(".js_close");
+import { renderHeader } from "./script.js";
 
-hamburgerBtn.addEventListener("click", ()=>{
-  subMenuEl.style.display = "initial";
-  hamburgerBtn.style.display = "none";
-  cancelBtn.style.display = "initial";
-})
-
-cancelBtn.addEventListener("click", ()=>{
-  subMenuEl.style.display = "none";
-  hamburgerBtn.style.display = "initial";
-  cancelBtn.style.display = "none";
-
-})
+renderHeader();
